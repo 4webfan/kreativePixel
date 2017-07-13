f18436091a8d93d62611b521df00bb259ae4656a
Aleksey <dovbnya@intaro.ru>
2017-07-13 11:36:11 +0300

    new markup

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
-$(function () {
+/*$(function () {
     Main.init();
 
 
 
-});
-Main = {
+});*/
+/*Main = {
     init: function(){
         
     }
    
-}
+}*/
+
+///////////////////////////
+
+document.querySelector('.burger').addEventListener('click', function(e){
+    this.classList.toggle('burger_active');
+
+    document.querySelector('.main-nav').classList.toggle('main-nav_active');
+
+}, false);
+//////////////////////////
 
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 8,
-        center: {lat: -34.397, lng: 150.644}
+        zoom: 12,
+        center: {lat: 55.45, lng: 37.36}
     });
     var geocoder = new google.maps.Geocoder();
 
